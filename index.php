a12dedfccb1fb5c19bb85e5386a042a3108c60ed
sumansekar <43912131+sumansekar@users.noreply.github.com>
2019-01-08 23:38:56 -0600

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,8 @@ if ($method == 'POST')
     $requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
 	$LanID = $json->result->parameters->LanID;
+	$username = 'Administrator';
+	$password = 'manage';
 	$URL = "http://localhost:5555/invoke/Default:new_flowservice?num1=2&num2=4";
 
     
@@ -22,6 +24,39 @@ if ($method == 'POST')
             $Status_MSG = "Your Cognizant  ID is not valid. Thanks";
             break;
     }
+//HTTP options
+
+$opts = array('http' =>
+
+    array(
+
+        'method'    => 'POST',
+
+        'header'    => array ('Content-type: application/json', 'Authorization: Basic '.base64_encode("$username:$password")),
+
+        'content' => "some_content"
+
+    )
+
+);
+
+
+
+//Do request
+
+$context = stream_context_create($opts);
+
+$jsonStr = file_get_contents($URL, false, $context);
+
+
+
+    	$obj = json_decode($jsonStr,true);
+	$json1 = $obj['response'];
+$json =json_decode($json1);
+	$stdResponse="Nothing :-";
+$message = '';
+$message = ($json->errormessage == "null" OR empty($json->errormessage) )?"N/A":$json->errormessage;
+$Status_MSG = " Status=".$json->status.";"." Message=".$message;
 
     $response = new \stdClass();
     $response->speech = "abcd" . $Status_MSG . $LanID;
