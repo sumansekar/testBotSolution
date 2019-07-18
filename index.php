e458987f9751af9def64e708cfc8c87d58174942
sumansekar <43912131+sumansekar@users.noreply.github.com>
2019-07-18 07:09:15 -0500

    Update index.php

--- index.php
+++ index.php
@@ -20,7 +20,17 @@ if ($method == 'POST')
         
         case "sguna002":
 		  
-		        $Status_MSG = "Your Cognizant ID $LanID has been validated. Thanks";
+		      //  $Status_MSG = "Your Cognizant ID $LanID has been validated. Thanks";
+		     $URL="http://66.25.18.67:5555/rest/Default/new_restExp/_get?num1=2&num2=4";
+$context = stream_context_create(array(
+'http' => array(
+
+       'header'    => ['Content-type: application/json' , 'Accept: application/json', 'Authorization: Basic '.base64_encode("$username:$password")], 'method'    => 'POST)));
+
+$jsonStr = file_get_contents($URL, false, $context);
+$obj = json_decode($jsonStr,true);
+$Status_MSG = $obj->{'sumTotal'};
+
 		    	  break;
             
         default:
